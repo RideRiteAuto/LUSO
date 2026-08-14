9a752574378d51ea33997357d9317f6002b1ccd1
RideRiteAuto <Kevin@riderite.us>
2026-08-13 19:09:50 -0700

    Publish seed 48291 (76147d18ff00)

--- navora-cache-worker.js
+++ navora-cache-worker.js
@@ -1,5 +1,5 @@
 
-const CURRENT_CACHE = "navora-world-48291-2db448a586fb";
+const CURRENT_CACHE = "navora-world-48291-76147d18ff00";
 const CACHE_PREFIX = "navora-world-";
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", event => event.waitUntil((async () => {
